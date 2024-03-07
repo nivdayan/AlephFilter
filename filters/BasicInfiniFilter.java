3e1aa5f5f49d7065d7ff65ccf9cd5967c504b558
“Niv <nivdayan@gmail.com>
2024-03-06 20:54:41 -0500

    adding experiment base class

--- filters/BasicInfiniFilter.java
+++ filters/BasicInfiniFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Niv Dayan
+ * Copyright 2024 Niv Dayan
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
