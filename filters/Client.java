a16e060a344c26678c9125d0c9d3642e25fd11ed
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2024-03-18 19:37:59 -0400

    exp

--- filters/Client.java
+++ filters/Client.java
@@ -16,11 +16,11 @@ limitations under the License.
 
 package filters;
 
-//import infiniFilter_experiments.Experiment1;
-//import infiniFilter_experiments.Experiment2;
-//import infiniFilter_experiments.Experiment3;
-//import infiniFilter_experiments.Experiment4;
-//import infiniFilter_experiments.ExperimentsBase;
+import infiniFilter_experiments.Experiment1;
+import infiniFilter_experiments.Experiment2;
+import infiniFilter_experiments.Experiment3;
+import infiniFilter_experiments.Experiment4;
+import infiniFilter_experiments.ExperimentsBase;
 
 public class Client {
 
@@ -36,22 +36,22 @@ public class Client {
 		Tests.test8(); // expansion test for FingerprintShrinkingQF
 		Tests.test9(); // expansion test for MultiplyingQF
 		Tests.test10(); // testing InfiniFilter
-		//Tests.test12(); // testing InfiniFilter - chained
-		//Tests.test13(); // testing InfiniFilter - rejuvenation
-		//Tests.test14(); // InfiniFilter deleting largest matching fingerprint
-		//Tests.test15(); // testing deletes
+		Tests.test12(); // testing InfiniFilter - chained
+		Tests.test13(); // testing InfiniFilter - rejuvenation
+		Tests.test14(); // InfiniFilter deleting largest matching fingerprint
+		Tests.test15(); // testing deletes
 		Tests.test16(); // testing deletes
 		Tests.test17(); // testing deletes
-		//Tests.test18(); // testing deletes & rejuv operations
-		//Tests.test19(); // testing xxhash
-		////Tests.test20(1000000); //testing xxhash(ByteBuffer)==xxhash(long)
-		//Tests.test21(1000000); // testing insert,search an delete of types int,long,String,byte[]
+		Tests.test18(); // testing deletes & rejuv operations
+		Tests.test19(); // testing xxhash
+		//Tests.test20(1000000); //testing xxhash(ByteBuffer)==xxhash(long)
+		Tests.test21(1000000); // testing insert,search an delete of types int,long,String,byte[]
 		Tests.test22(); // testing no false negatives for bloom filter
 		Tests.test23(); // no false negatives for cuckoo filter
 		//Tests.test24(); // testing false positive rate for quotient filter
 		//Tests.test25(); // testing false positive rate for cuckoo filter
 		//Tests.test26(); // testing false positive rate for bloom filter
-		//Tests.test27(); // exceeding the bound of the quotient filter
+		Tests.test27(); // exceeding the bound of the quotient filter
 		
 		System.out.println("all tests passed");
 	}
